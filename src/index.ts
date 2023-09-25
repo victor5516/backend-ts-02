13b18e3523292a66515cf92e28eb0915b9958005
Juan Cardenas <cardenasriosjuandavid@gmail.com>
2023-09-25 06:40:40 -0500

    feat: :sparkles: New endpoints for search and create books

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
-// importamos la libreria express
-import express from 'express';
-// creamos una instancia de express
-const app = express();
-// definimos el puerto por el cual va a escuchar nuestro servidor
-const port = 3000;
-// definimos una ruta de tipo GET
-app.get('/', (req, res) => {
-  res.send('HELLO TS');
-});
+// Importamos la librería express
+import express from 'express'
+// Creamos una instancia de express
+const app = express()
+
+// Configuración para el servidor
+app.use(express.json())
+
+// Definimos el puerto por el cual va a escuchar nuestro servidor
+const port = 3000
+
+// Biblioteca
+const library = [
+  {
+    title: 'Lord of the rings',
+    author: 'J. R. R. Tolkien'
+  }
+]
+
+// Obtener todos los libros
+app.get('/book', (req, res) => {
+  res.json(library)
+})
+
+// Crear libro
+app.post('/book', (req, res) => {
+  // Captura de información
+  const book = req.body
+
+  // Inserción del libro a la biblioteca
+  library.push(book)
+
+  res.status(201).json({
+    message: `Libro almacenado correctamente: ${book.title} de ${book.author}`
+  })
+})
+
 // levantamos el servidor en el puerto especificado
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
